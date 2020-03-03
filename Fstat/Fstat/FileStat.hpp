afca4fbde5755e816f5503dc16c209293f60848a
Arthur Pons <unguestdev@gmail.com>
2020-03-03 01:29:16 +0100

    First Commit
    I started to work on this project 1h ago. Too late to debug the error in the readfile func => Use an existent one ?

---
+++ Fstat/Fstat/FileStat.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class FileStat
+{
+private:
+	std::string filePath;
+	std::string fileContent;
+	int numberOfChars;
+	int numberOfLines;
+	int numberOfSpaces;
+
+public:
+	FileStat(std::string* filename);
+	~FileStat();
+	std::string ReadAllFile();
+	int getNumberOfChars();
+	int getNumberOfLines();
+	void PrintStats();
+};
+
